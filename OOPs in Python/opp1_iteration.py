365324d1339c0ad17144542443d294b56de0368f
Preeti123Sharma1 <108971201+Preeti123Sharma1@users.noreply.github.com>
2023-08-09 23:02:05 +0530

    update

--- OOPs in Python/opp1_iteration.py
+++ OOPs in Python/opp1_iteration.py
@@ -5,3 +5,9 @@ a = len(w)
 print(a)
 for t in range(a):
     print(w[t])
+
+
+print(" ")
+
+for t in range(a-1,-1,-1):              # (total index -1, -1 means we go to 0 index, we have to decrement -1)
+    print(w[t])
---
+++ OOPs in Python/opp1_iteration2.py
@@ -0,0 +1,5 @@
+w = 'Welcome to Lucknow'
+w = w[-1::-1]
+
+for t in w:
+    print(t)
