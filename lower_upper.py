bd2b9759fd7e7fddcaee119f75822431208cd457
Preeti123Sharma1 <108971201+Preeti123Sharma1@users.noreply.github.com>
2023-08-10 23:25:17 +0530

    Create lower_upper.py

---
+++ lower_upper.py
@@ -0,0 +1,15 @@
+w = "Hello, welcome to Lucknow"
+
+a = w.lower()
+print("For lower case: ",a)
+
+b = w.upper()
+print("For upper case: ",b)
+
+t = w.title()
+print("For title case: ",t)
+
+p = "hello, welcome to lucknow"
+
+c = w.capitalize()
+print("For capitalize case: ",c) 
